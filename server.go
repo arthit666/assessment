a54c09a78874c8c21d17d16574c5b744b893c8f6
Arthit666 <arthit0ak@gmail.com>
2022-12-27 15:01:04 +0700

    graceful shutdown

--- server.go
+++ server.go
@@ -1,8 +1,13 @@
 package main
 
 import (
-	"log"
+	"context"
+	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -23,7 +28,23 @@ func main() {
 	e.PUT("/expenses/:id", expenses.PutExpenses)
 	e.GET("/expenses", expenses.GetAllExpanses)
 
-	log.Println("sever start at port:", os.Getenv("PORT"))
-	log.Fatal(e.Start(os.Getenv("PORT")))
+	go func() {
+		if err := e.Start(os.Getenv("PORT")); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
 
+	fmt.Println("sever start at port:", os.Getenv("PORT"))
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	<-shutdown
+	fmt.Println("shutting down...")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+	fmt.Println("bye bye")
 }
